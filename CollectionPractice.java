973267b12e78e560b2f51e9f2283055b876df73b
dds199 <dds1999@users.noreply.github.com>
2017-10-29 14:35:10 -0400

    Update 10/29/2017

---
+++ CollectionPractice.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class CollectionPractice {
+    public static void main(String[] args) {
+        //set : no dublicates
+        //list : list of items, duplicates ok
+        //queue : first in, first out
+        //deque : delete from front and back
+        //map : key - value pair
+        List list = new ArrayList();
+        list.add("hello");
+        list.add(4);
+        System.out.println(list);
+    }
+}
