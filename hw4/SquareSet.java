24892e713a41ee9213150d0e733e1567f4c85bc3
dds1999 <dds1999@users.noreply.github.com>
2017-10-29 21:47:00 -0400

    HW 4 started

---
+++ hw4/SquareSet.java
@@ -0,0 +1,9 @@
+import java.util.Collection;
+/**
+ * This class outlines a Collections Set object capable of holding Squares
+ * @author dds7
+ * @version 9.0.1
+ */
+public class SquareSet implements Set {
+
+}
